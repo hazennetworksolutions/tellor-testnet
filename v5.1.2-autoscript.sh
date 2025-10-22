c9d58d4c966a1dd256d92be055574f7c53096f47
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2025-10-22 19:24:14 +0300

    Update v5.1.2-autoscript.sh

--- v5.1.2-autoscript.sh
+++ v5.1.2-autoscript.sh
@@ -1,1 +1,258 @@
+#!/bin/bash
+LOG_FILE="/var/log/layer_node_install.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
 
+printGreen() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+printLine() {
+    echo "------------------------------"
+}
+
+# Function to print the node logo
+function printNodeLogo {
+    echo -e "\033[32m"
+    echo "          
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+█████████████████████████████████████                          █████████████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+██████████████████████████████             █             █            ██████████████████████████████
+████████████████████████████           █████             ████           ████████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ███████            ██████          ███████████████████████████
+████████████████████████████          ██████████         ██████          ███████████████████████████
+████████████████████████████          █████████████      ██████          ███████████████████████████
+████████████████████████████             █████████████     ████          ███████████████████████████
+████████████████████████████          █     █████████████     █          ███████████████████████████
+████████████████████████████          █████     ████████████             ███████████████████████████
+████████████████████████████          ██████       ████████████          ███████████████████████████
+████████████████████████████          ██████          █████████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████            ████             ███            ████████████████████████████
+██████████████████████████████                                        ██████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+█████████████████████████████████████                           ████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+Hazen Network Solutions 2025 All rights reserved."
+    echo -e "\033[0m"
+}
+
+# Show the node logo
+printNodeLogo
+
+# User confirmation to proceed
+echo -n "Type 'yes' to start the installation Tellor Testnet v5.1.2 and press Enter: "
+read user_input
+
+if [[ "$user_input" != "yes" ]]; then
+  echo "Installation cancelled."
+  exit 1
+fi
+
+# Function to print in green
+printGreen() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+printGreen "Starting installation..."
+sleep 1
+
+printGreen "If there are any, clean up the previous installation files"
+
+sudo systemctl stop layerd
+sudo systemctl disable layerd
+sudo rm -rf /etc/systemd/system/layerd.service
+sudo rm $(which layerd)
+sudo rm -rf $HOME/.layer
+sed -i "/LAYER_/d" $HOME/.bash_profile
+
+# Update packages and install dependencies
+printGreen "1. Updating and installing dependencies..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip screen btop iotop nethogs hdparm -y
+sudo apt install -y curl git jq lz4 build-essential cmake perl automake autoconf libtool wget libssl-dev -y
+
+# User inputs
+read -p "Enter your MONIKER: " MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (2-digit): " PORT
+echo 'export PORT='$PORT
+
+# Setting environment variables
+echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+echo "export LAYER_CHAIN_ID=\"layertest-4\"" >> $HOME/.bash_profile
+echo "export LAYER_PORT=$PORT" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Chain ID:       \e[1m\e[32m$LAYER_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$LAYER_PORT\e[0m"
+printLine
+sleep 1
+
+# Install Go
+printGreen "2. Installing Go..." && sleep 1
+cd $HOME
+VER="1.23.0"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo 'export PATH=/usr/local/go/bin:$HOME/go/bin:$PATH' >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+export PATH="$HOME/go/bin:$PATH"
+
+# Version check
+echo $(go version) && sleep 1
+
+# Download Prysm protocol binary
+printGreen "3. Downloading Tellor binary and setting up..." && sleep 1
+
+cd $HOME
+mkdir -p $HOME/.layer/cosmovisor/upgrades/v5.1.2/bin
+
+wget https://github.com/tellor-io/layer/releases/download/v5.1.2/layer_Linux_x86_64.tar.gz
+tar -xvf layer_Linux_x86_64.tar.gz
+
+sudo mv layerd $HOME/.layer/cosmovisor/upgrades/v5.1.2/bin/
+chmod +x $HOME/.layer/cosmovisor/upgrades/v5.1.2/bin/layerd
+
+sudo ln -sfn $HOME/.layer/cosmovisor/upgrades/v5.1.2 $HOME/.layer/cosmovisor/current
+sudo ln -sfn $HOME/.layer/cosmovisor/current/bin/layerd /usr/local/bin/layerd
+
+
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.6.0
+
+# Create service file
+sudo bash -c "cat > /etc/systemd/system/layerd.service" << EOF
+[Unit]
+Description=layer node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start --home $HOME/.layer
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.layer"
+Environment="DAEMON_NAME=layerd"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.layer/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable layerd
+
+# Initialize the node
+printGreen "7. Initializing the node..."
+layerd config node tcp://localhost:${LAYER_PORT}657
+layerd config keyring-backend os
+layerd config chain-id layertest-4
+layerd init $MONIKER --chain-id layertest-4
+
+# Download genesis and addrbook files
+printGreen "8. Downloading genesis and addrbook..."
+wget -O $HOME/.layer/config/genesis.json https://raw.githubusercontent.com/hazennetworksolutions/tellor-testnet/refs/heads/main/genesis.json
+wget -O $HOME/.layer/config/addrbook.json https://raw.githubusercontent.com/hazennetworksolutions/tellor-testnet/refs/heads/main/addrbook.json
+
+
+# Configure gas prices and ports
+printGreen "9. Configuring custom ports and gas prices..." && sleep 1
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0loya"|g' $HOME/.layer/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.layer/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.layer/config/config.toml
+
+sed -i.bak -e "s%:1317%:${LAYER_PORT}317%g;
+s%:8080%:${LAYER_PORT}080%g;
+s%:9090%:${LAYER_PORT}090%g;
+s%:9091%:${LAYER_PORT}091%g;
+s%:8545%:${LAYER_PORT}545%g;
+s%:8546%:${LAYER_PORT}546%g;
+s%:6065%:${LAYER_PORT}065%g" $HOME/.layer/config/app.toml
+
+# Configure P2P and ports
+sed -i.bak -e "s%:26658%:${LAYER_PORT}658%g;
+s%:26657%:${LAYER_PORT}657%g;
+s%:6060%:${LAYER_PORT}060%g;
+s%:26656%:${LAYER_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${LAYER_PORT}656\"%;
+s%:26660%:${LAYER_PORT}660%g" $HOME/.layer/config/config.toml
+
+# Set up seeds and peers
+printGreen "10. Setting up peers and seeds..." && sleep 1
+SEEDS="c7b175a5bafb35176cdcba3027e764a0dbd0811c@34.219.95.82:26656,05105e8bb28e8c5ace1cecacefb8d4efb0338ec6@18.218.114.74:26656,705f6154c6c6aeb0ba36c8b53639a5daa1b186f6@3.80.39.230:26656,1f6522a346209ee99ecb4d3e897d9d97633ae146@3.101.138.30:26656,3822fa2eb0052b36360a7a6e285c18cc92e26215@175.41.188.192:26656"
+PEERS="c7b175a5bafb35176cdcba3027e764a0dbd0811c@34.219.95.82:26656,05105e8bb28e8c5ace1cecacefb8d4efb0338ec6@18.218.114.74:26656,705f6154c6c6aeb0ba36c8b53639a5daa1b186f6@3.80.39.230:26656,1f6522a346209ee99ecb4d3e897d9d97633ae146@3.101.138.30:26656,3822fa2eb0052b36360a7a6e285c18cc92e26215@175.41.188.192:26656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" \
+       $HOME/.layer/config/config.toml
+
+# Pruning Settings
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.layer/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.layer/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.layer/config/app.toml
+
+# Download the snapshot
+# printGreen "12. Downloading snapshot and starting node..." && sleep 1
+
+
+
+
+
+# Start the node
+printGreen "13. Starting the node..."
+sudo systemctl restart layerd
+
+# Check node status
+printGreen "14. Checking node status..."
+sudo journalctl -u layerd -f -o cat
+
+# Verify if the node is running
+if systemctl is-active --quiet layerd; then
+  echo "The node is running successfully! Logs can be found at /var/log/layer_node_install.log"
+else
+  echo "The node failed to start. Logs can be found at /var/log/layer_node_install.log"
+fi
